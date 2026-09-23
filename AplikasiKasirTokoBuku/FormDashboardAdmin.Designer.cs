009459d9af9927cf831dc0878f091f8e68fcbf91
ziroone12 <rafiffairuz139@gmail.com>
2026-09-23 11:45:05 +0700

    tygygy

--- AplikasiKasirTokoBuku/FormDashboardAdmin.Designer.cs
+++ AplikasiKasirTokoBuku/FormDashboardAdmin.Designer.cs
@@ -2,7 +2,7 @@
 {
     partial class FormDashboardAdmin
     {
-        /// <summary>
+        /// 
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
